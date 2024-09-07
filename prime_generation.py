22ad90d6ef3204e48e4abbae9cfaf947a508a4aa
Lee Hagaman <hagamanlee@gmail.com>
2024-09-07 17:33:54 -0500

    cleaned up model generation

--- prime_generation.py
+++ prime_generation.py
@@ -3,7 +3,11 @@ import numpy as np
 import ollama
 import time
 import pickle
-import subprocess
+from contextlib import nullcontext
+import torch
+from model import GPTConfig, GPT
+import warnings
+from prime_functions import convert_toks_to_nums, convert_nums_to_toks
 
 
 def get_llama_primes(prompt, max_output_count=None, print_stream=False, delay=0.05, disable_cache=False):
@@ -50,7 +54,6 @@ def recalculate_llama_primes(prompt, max_output_count, print_stream, delay):
     response_str = ""
     if print_stream: print("\nPrinting llama response stream:")
     num_complete_numbers_output = 0
-    #for part in ollama.generate('llama3:8b-text-fp16', prompt, stream=True):
     try:
         for part in ollama.generate('llama3_8b_text_fp16_zero_seed_zero_temp:latest', prompt, stream=True):
             if max_output_count != None and num_complete_numbers_output >= max_output_count:
@@ -81,16 +84,55 @@ def recalculate_llama_primes(prompt, max_output_count, print_stream, delay):
     return np.array(nums_str)
 
 
-def get_model_primes(max_new_tokens, temperature, seed_primes):
-    # running this in a subprocess, since my cuda environment exists as the command line python
-    env = os.environ.copy()
-    command = f"python test.py --max_new_tokens {max_new_tokens} --temperature {temperature} --seed_primes {' '.join(map(str, seed_primes))}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True, env=env)
-    lines = result.stdout.splitlines()
-    if not lines:
-        print("get_model_primes error output:", result.stderr)
+def get_model_primes(max_output_count, max_new_tokens, temperature, seed_primes):
+    # modified from https://github.com/karpathy/nanoGPT/blob/master/sample.py
 
-    primes_str = lines[1]
-    primes = [int(s) for s in primes_str.split(",")[:-1]]
-    return np.array(primes)
+    out_dir = 'out'
+
+    top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
+    seed = 1337
+    device = 'cuda'
+    dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
+    compile = True
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+    torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+    device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+    ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+    ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+
+    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+    warnings.filterwarnings("ignore", category=FutureWarning, message="You are using `torch.load` with `weights_only=False`")
+    checkpoint = torch.load(ckpt_path, map_location=device)
+    gptconf = GPTConfig(**checkpoint['model_args'])
+    model = GPT(gptconf)
+    state_dict = checkpoint['model']
+    unwanted_prefix = '_orig_mod.'
+    for k, v in list(state_dict.items()):
+        if k.startswith(unwanted_prefix):
+            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+    model.load_state_dict(state_dict)
+
+    model.eval()
+    model.to(device)
+    if compile:
+        model = torch.compile(model) # requires PyTorch 2.0 (optional)
+
+    seed_prime_toks = convert_nums_to_toks(seed_primes)
+
+    x = torch.tensor(seed_prime_toks, dtype=torch.long, device=device)[None, ...]
+
+    with torch.no_grad():
+        with ctx:
+            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+            nums = convert_toks_to_nums(y[0].tolist()[len(seed_prime_toks):])
+
+    if max_output_count != None and len(nums) > max_output_count:
+        nums = nums[:max_output_count]
+            
+    return np.array(nums)
+
+
 
